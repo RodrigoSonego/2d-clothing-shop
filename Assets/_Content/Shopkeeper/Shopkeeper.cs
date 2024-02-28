a65049a7f10185198c9c5ecdf10b35a075e6764f
Rodrigo Sonego <rodrigoasonego@gmail.com>
2024-02-28 01:48:21 -0300

    fix: tile palette broken

--- Assets/_Content/Shopkeeper/Shopkeeper.cs
+++ Assets/_Content/Shopkeeper/Shopkeeper.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Shopkeeper : MonoBehaviour
 {
 	[SerializeField] private Shop shop;
+	[SerializeField] GameObject speechBubble;
 
 	private void Start()
 	{
 		shop.OnShopClose += FinishInteraction;
+
+		speechBubble.SetActive(false);
 	}
 
 	private void OpenShop()
@@ -25,11 +28,15 @@ public class Shopkeeper : MonoBehaviour
 		Player.Instance.SetCanInteract(true);
 
 		Player.Instance.Interact += OpenShop;
+
+		speechBubble.SetActive(true);
 	}
 
 	private void OnTriggerExit2D(Collider2D collision)
 	{
 		Player.Instance.SetCanInteract(false);
 		Player.Instance.Interact -= OpenShop;
+
+		speechBubble.SetActive(false);
 	}
 }
